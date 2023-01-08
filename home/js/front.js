9f5eb81fe91f6870a5f36a30fbd3ba8e39835aff
Utsanjan Maity <utsanjan@gmail.com>
2023-01-08 10:14:58 +0530

    initial update

--- home/js/front.js
+++ home/js/front.js
@@ -4,13 +4,11 @@ function resizeForm(){
     var width=(window.innerWidth>0)?window.innerWidth:document.documentElement.clientWidth;
     if(width > 1024){document.getElementById("social-menu").checked = true;
     document.getElementById("social-menu").style.visibility = "hidden";
-    document.getElementById("menu").style.marginTop = "-70px";
     document.getElementById("ham1").style.visibility = "hidden";
     document.getElementById("ham2").style.visibility = "hidden";
     document.getElementById("ham3").style.visibility = "hidden"} 
     else {document.getElementById("social-menu").checked = false;
     document.getElementById("social-menu").style.visibility = "visible";
-    document.getElementById("menu").style.marginTop = "-100px";
     document.getElementById("ham1").style.visibility = "visible";
     document.getElementById("ham2").style.visibility = "visible";
     document.getElementById("ham3").style.visibility = "visible"}    
